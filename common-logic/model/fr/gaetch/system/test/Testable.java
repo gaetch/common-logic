e5a246586744f843de6891c22c0182bd95bf7d31
gaetch <gaetch@hush.ai>
2013-12-06 14:34:20 +0100

    Removing interfaces Tester and Testable, favor a code coverage tool (later)

--- common-logic/model/fr/gaetch/system/test/Testable.java
+++
@@ -1,5 +0,0 @@
-package fr.gaetch.system.test;
-
-public interface Testable {
-	public abstract Class<?> getTester();
-}
--- common-logic/model/fr/gaetch/system/test/Tester.java
+++
@@ -1,5 +0,0 @@
-package fr.gaetch.system.test;
-
-public interface Tester {
-	public abstract Class<?> getTested();
-}
